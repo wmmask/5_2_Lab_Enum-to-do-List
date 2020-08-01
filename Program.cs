728205e4b9bb26dd1a69f140081fca26fec21cb1
wmmask <wf1029@post.com>
2020-07-31 21:38:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5_2_Lab_Enum_to_do_List
+{
+    class Program
+    {
+        // defining enum - days of the week
+        public enum WeekDays { 
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Create a list to hold tasks
+            List<string> tasklist = new List<string>();
+            //Loop through each week day so user can enter task
+            foreach (WeekDays   day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("Enter a new task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                tasklist.Add(task);
+            }
+            //Print out to screen the task list.
+            foreach (string task in tasklist)
+            {
+                Console.WriteLine(task);
+            }
+
+        }
+    }
+}
